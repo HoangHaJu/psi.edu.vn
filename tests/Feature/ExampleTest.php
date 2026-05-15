5661ef0ff7bb9021cc351f472f177fbeaa5a7e91
HoangHaJu <130366982+HoangHaJu@users.noreply.github.com>
2026-05-16 00:15:59 +0700

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,16 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase; // Thêm dòng này
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase; // Thêm dòng này để Laravel tự lo phần Database khi test
+
     public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
